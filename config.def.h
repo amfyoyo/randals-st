b26aa0ebb4476a525786ec0c90145cb33eb5c41f
amfyoyo <randal.helm@outlook.com>
2023-08-11 13:34:02 -0700

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.88, alphaUnfocused = 0.65;
+float alpha = 0.87, alphaUnfocused = 0.63;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
